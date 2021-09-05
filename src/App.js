ab21a14c951ae132ea7b33590d7b9b1764714279
developer.be.3 <developer.be.3@yeganeh.local>
2021-09-05 19:34:39 +0430

    Arrow function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import SayHello from './Components/FuncComponent';
+import SayHello, { SayHello2 } from './Components/FuncComponent';
 
 function App() {
   return (
     <div className="App">
      <h1>Explicit Hello from within the app page</h1>
      <SayHello></SayHello>
+     <SayHello2></SayHello2>
     </div>
   );
 }
