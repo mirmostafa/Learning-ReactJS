fdbafbde816d045469fbf2cdc4906a53ae880c4a
developer.be.3 <developer.be.3@yeganeh.local>
2021-09-06 17:55:12 +0430

    Organize Files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import SayHello, { SayHello2 } from './Components/FuncComponent';
+import SayHello, { SayHello2 } from './Lessons/0500-Functional Component';
+import Hello from './Lessons/0600-Class Component';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
      <h1>Explicit Hello from within the app page</h1>
      <SayHello></SayHello>
      <SayHello2></SayHello2>
+     <Hello></Hello>
     </div>
   );
 }
--- src/Lessons/0600-Class Component.jsx
+++ src/Lessons/0600-Class Component.jsx
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+
+
 class Hello extends Component {
     render = () => <h1>Hello from class</h1>
 }
+
+
+export default Hello
