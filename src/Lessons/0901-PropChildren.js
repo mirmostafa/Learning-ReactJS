54c413d61c1475d23efce18407f6fd807ac9589e
developer.be.3 <developer.be.3@yeganeh.local>
2021-09-06 19:33:08 +0430

    Checkpoint

--- src/Lessons/0901-PropChildren.js
+++ src/Lessons/0901-PropChildren.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const PropsChildren = props => {
     console.log({ props });
-    return (<h1>Hello {props.children} from Children</h1>);
+    return (
+    <div>
+      <h1>Hello to children</h1> {props.children} <h1>from daddy</h1>
+    </div>
+    );
 }
 
 export default PropsChildren;
