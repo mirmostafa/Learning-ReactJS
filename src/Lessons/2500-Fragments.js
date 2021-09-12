ad0db4398958815d83134a049ccffd0166e29ef7
developer.be.3 <developer.be.3@yeganeh.local>
2021-09-12 10:11:25 +0430

    Fragments

---
+++ src/Lessons/2500-Fragments.js
@@ -0,0 +1,14 @@
+import React, {
+    Component
+} from 'react'
+
+function FragmentDemos() {
+    return (
+        <React.Fragment>
+            <h1>Framgment Demo</h1>
+            <p>This is a sample sentence.</p>
+        </React.Fragment>
+    )
+}
+
+export default FragmentDemos
---
+++ src/Lessons/2501-EmptyTag.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function EmptyTag() {
+    return (
+        <>
+            <h1>Empty Tag Demo</h1>
+            <p>This is a sample sentence.</p>
+        </>
+    )
+}
+
+export default EmptyTag
